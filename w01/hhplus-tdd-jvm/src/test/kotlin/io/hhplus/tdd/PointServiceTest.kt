924cbfdbbed0ede16f5744512ff70f02ad89f100
taehyun <0417taehyun@gmail.com>
2025-05-20 22:34:31 +0900

    Add a successful test case

--- w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointServiceTest.kt
+++ w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointServiceTest.kt
@@ -92,4 +92,58 @@ class PointServiceTest {
         verify(userPointTable, never()).insertOrUpdate(id = userId, amount = existingPoint + amount)
         verify(pointHistoryTableMock, never()).insert(id = userId, amount = amount, transactionType = TransactionType.CHARGE, updateMillis = fakeUpdateMilliseconds)
     }
+
+    @Test
+    @DisplayName("Given 10,000 existing points, When using 10,000 points, Then left point becoming 0 successfully.")
+    fun givenExistingPoints_whenUsingPoints_ThenBalancingPointsSuccessfully() {
+        // Given
+        val userId = 1L
+        val existingPoint = 10_000L
+        val amount = 10_000L
+        val fakeUpdateMilliseconds = 1000L
+
+        val userPointTable = mock(UserPointTable::class.java)
+        val pointHistoryTableMock = mock(PointHistoryTable::class.java)
+        val fakeTimeUtil = FakeTimeUtil(fixedTime = fakeUpdateMilliseconds)
+
+        val pointService = PointService(userPointTable = userPointTable, pointHistoryTable = pointHistoryTableMock, timeUtil = fakeTimeUtil)
+
+        `when`(userPointTable.selectById(id = userId))
+            .thenReturn(UserPoint(id = userId, point = existingPoint, updateMillis = fakeUpdateMilliseconds))
+        `when`(userPointTable.insertOrUpdate(id = userId, amount = existingPoint - amount))
+            .thenReturn(UserPoint(id = userId, point = existingPoint - amount, updateMillis = fakeUpdateMilliseconds))
+        `when`(
+            pointHistoryTableMock.insert(
+                id = userId,
+                amount = amount,
+                transactionType = TransactionType.USE,
+                updateMillis = fakeUpdateMilliseconds
+            )
+        ).thenReturn(
+            PointHistory(
+                id = 1L,
+                userId = userId,
+                amount = amount,
+                type = TransactionType.USE,
+                timeMillis = fakeUpdateMilliseconds
+            )
+        )
+
+        // When
+        val result = pointService.use(userId = userId, amount = amount)
+
+        // Then
+        assertThat(result)
+            .isEqualTo(UserPoint(id = userId, point = existingPoint - amount, updateMillis = fakeUpdateMilliseconds))
+
+        verify(userPointTable, times(1)).selectById(id = userId)
+        verify(userPointTable, times(1)).insertOrUpdate(id = userId, amount = existingPoint - amount)
+        verify(pointHistoryTableMock, times(1))
+            .insert(
+                id = userId,
+                amount = amount,
+                transactionType = TransactionType.USE,
+                updateMillis = fakeUpdateMilliseconds
+            )
+    }
 }
