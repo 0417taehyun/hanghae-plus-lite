556b10983233e6b3d3282baab59de847d18d9b1c
taehyun <0417taehyun@gmail.com>
2025-05-20 22:35:42 +0900

    Add IllegalAmountUseException

--- w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointException.kt
+++ w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointException.kt
@@ -2,4 +2,5 @@ package io.hhplus.tdd.point
 
 sealed class PointException(message: String): Exception(message) {
     class IllegalAmountChargeException(message: String): PointException(message = message)
+    class IllegalAmountUseException(message: String): PointException(message = message)
 }
