bd24c7d3f8cb283a69ab56490d5467b19a257337
taehyun <0417taehyun@gmail.com>
2025-05-20 23:13:15 +0900

    Format numbers for redability

--- w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointServiceTest.kt
+++ w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointServiceTest.kt
@@ -206,7 +206,7 @@ class PointServiceTest {
         // Given
         val userId = 1L
         val amount = 10_000L
-        val fakeUpdateMilliseconds = 1000L
+        val fakeUpdateMilliseconds = 1_000L
 
         val userPointTable = mock(UserPointTable::class.java)
         val pointHistoryTableMock = mock(PointHistoryTable::class.java)
