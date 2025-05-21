ddd56c7d15f1cfbc46ca7cd8770ca28e74b620dc
taehyun <0417taehyun@gmail.com>
2025-05-21 23:08:35 +0900

    Add a test case

--- w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointControllerTest.kt
+++ w01/hhplus-tdd-jvm/src/test/kotlin/io/hhplus/tdd/PointControllerTest.kt
@@ -1,8 +1,8 @@
 package io.hhplus.tdd
 
-import io.hhplus.tdd.point.PointController
-import io.hhplus.tdd.point.PointService
-import io.hhplus.tdd.point.UserPoint
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.database.UserPointTable
+import io.hhplus.tdd.point.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -29,5 +29,29 @@ class PointControllerTest {
 
         // Then
         assertThat(result).isEqualTo(UserPoint(id = userId, point = point, updateMillis = fakeUpdateMilliseconds))
+    }
+
+    @Test
+    @DisplayName("Given two transactions, When passing matched user id, Then returning a history of two transactions successfully.")
+    fun givenExistingPointHistory_whenPassingMatchedUserId_ThenReturningListOfPointHistorySuccessfully() {
+        // Given
+        val userId = 1L
+        val amount = 10_000L
+        val fakeUpdateMilliseconds = 1_000L
+
+        val pointServiceMock = mock(PointService::class.java)
+
+        val pointController = PointController(pointServiceMock)
+
+        val chargeTransaction = PointHistory(id = 1L, userId = userId, type = TransactionType.CHARGE, amount = amount, timeMillis = fakeUpdateMilliseconds)
+        val useTransaction = PointHistory(id = 2L, userId = userId, type = TransactionType.USE, amount = amount, timeMillis = fakeUpdateMilliseconds)
+
+        `when`(pointServiceMock.getHistories(userId = userId)).thenReturn(listOf(chargeTransaction, useTransaction))
+
+        // When
+        val result = pointController.history(id = userId)
+
+        // Then
+        assertThat(result).isEqualTo(listOf(chargeTransaction, useTransaction))
     }
 }
