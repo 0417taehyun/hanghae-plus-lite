c0c87fc2df0b4f497ef00ff4e5e5ff8c5a28948e
taehyun <0417taehyun@gmail.com>
2025-05-22 22:44:56 +0900

    Override handleMethodArgumentNotValid to return custom message

--- w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/ApiControllerAdvice.kt
+++ w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/ApiControllerAdvice.kt
@@ -2,10 +2,14 @@ package io.hhplus.tdd
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 data class ErrorResponse(val code: String, val message: String)
@@ -13,6 +17,24 @@ data class ErrorResponse(val code: String, val message: String)
 @RestControllerAdvice
 class ApiControllerAdvice : ResponseEntityExceptionHandler() {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val errorMessage = ex.bindingResult
+            .fieldErrors
+            .map {
+                mapOf(
+                    "field" to it.field,
+                    "details" to (it.defaultMessage ?: "유효하지 않은 필드입니다.")
+                )
+            }
+
+        return ResponseEntity(mapOf("code" to "400", "message" to errorMessage), HttpStatus.BAD_REQUEST)
+    }
 
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
