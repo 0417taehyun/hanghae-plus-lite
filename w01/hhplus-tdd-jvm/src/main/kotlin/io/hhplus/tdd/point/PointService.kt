d9f8bdaec9b756141bd87be2f7e475072803c889
taehyun <0417taehyun@gmail.com>
2025-05-23 09:56:53 +0900

    Adjust LockManager to PointService

--- w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ w01/hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point
 
+import io.hhplus.tdd.common.LockManager
 import io.hhplus.tdd.common.TimeUtil
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
@@ -9,42 +10,52 @@ import org.springframework.stereotype.Service
 class PointService(
     private val userPointTable: UserPointTable,
     private val pointHistoryTable: PointHistoryTable,
-    private val timeUtil: TimeUtil
+    private val timeUtil: TimeUtil,
+    private val lockManager: LockManager,
 ) {
     fun charge(userId: Long, amount: Long): UserPoint {
-        val existingUserPoint = userPointTable.selectById(id = userId)
-        val totalAmount = existingUserPoint.point + amount
+        return lockManager.withLock(userId = userId) {
+            val existingUserPoint = userPointTable.selectById(id = userId)
+            val totalAmount = existingUserPoint.point + amount
 
-        if (totalAmount > MAXIMUM_POINT) {
-            throw PointException.IllegalAmountChargeException("Total point must be less than $MAXIMUM_POINT.")
+            if (totalAmount > MAXIMUM_POINT) {
+                throw PointException.IllegalAmountChargeException("Total point must be less than $MAXIMUM_POINT.")
+            }
+
+            val updatedUserPoint = userPointTable.insertOrUpdate(id = userId, amount = totalAmount)
+            pointHistoryTable.insert(id = userId, amount = amount, transactionType = TransactionType.CHARGE, updateMillis = timeUtil.getCurrentTimeInMilliSeconds())
+
+            updatedUserPoint
         }
-
-        val updatedUserPoint = userPointTable.insertOrUpdate(id = userId, amount = totalAmount)
-        pointHistoryTable.insert(id = userId, amount = amount, transactionType = TransactionType.CHARGE, updateMillis = timeUtil.getCurrentTimeInMilliSeconds())
-
-        return updatedUserPoint
     }
 
     fun use(userId: Long, amount: Long): UserPoint {
-        val existingUserPoint = userPointTable.selectById(id = userId)
-        val balancedAmount = existingUserPoint.point - amount
+        return lockManager.withLock(userId = userId) {
+            val existingUserPoint = userPointTable.selectById(id = userId)
+            val balancedAmount = existingUserPoint.point - amount
 
-        if (balancedAmount < 0) {
-            throw PointException.IllegalAmountUseException("Amount must be less than existing point.")
+            if (balancedAmount < 0) {
+                throw PointException.IllegalAmountUseException("Amount must be less than existing point.")
+            }
+
+            val updatedUserPoint = userPointTable.insertOrUpdate(id = userId, amount = balancedAmount)
+            pointHistoryTable.insert(id = userId, amount = amount, transactionType = TransactionType.USE, updateMillis = timeUtil.getCurrentTimeInMilliSeconds())
+
+            updatedUserPoint
         }
 
-        val updatedUserPoint = userPointTable.insertOrUpdate(id = userId, amount = balancedAmount)
-        pointHistoryTable.insert(id = userId, amount = amount, transactionType = TransactionType.USE, updateMillis = timeUtil.getCurrentTimeInMilliSeconds())
-
-        return updatedUserPoint
     }
 
     fun get(userId: Long): UserPoint {
-        return userPointTable.selectById(id = userId)
+        return lockManager.withLock(userId = userId) {
+            userPointTable.selectById(id = userId)
+        }
     }
 
     fun getHistories(userId: Long): List<PointHistory> {
-        return pointHistoryTable.selectAllByUserId(userId = userId)
+        return lockManager.withLock(userId = userId) {
+            pointHistoryTable.selectAllByUserId(userId = userId)
+        }
     }
 
     companion object {
